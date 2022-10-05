01cc445605fc15c1739493edfb570bf2ac719d3e
Anna <anna.skorohodova@inbox.ru>
2022-10-06 00:39:23 +0300

    task1 correct

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,7 +3,8 @@
 Console.WriteLine("Введите трехзначное число");
 string input = Console.ReadLine();
 double number = double.Parse(input);
-if (number > 99 && number < 1000) {
+if (number > 99 && number < 1000)
+{
   double result = (number / 10 - number % 10/10) % 10;
   Console.WriteLine(result);
 }
@@ -16,7 +17,8 @@ else
 Console.WriteLine("Введите трехзначное число");
 string input1 = Console.ReadLine();
 double number1 = double.Parse(input1);
-if (number1 > 99 && number1 < 1000) {
+if (number1 > 99 && number1 < 1000)
+{
   Console.WriteLine(input1[1]);
 }
 else
