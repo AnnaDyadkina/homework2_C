91119436f1f51b10eb963642cced1128dfbf9586
Anna <anna.skorohodova@inbox.ru>
2022-10-07 00:17:31 +0300

    В задаче 2 добавлена проверка на отрицательное число

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,6 +1,9 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
 //Console.WriteLine("Hello, World!");
 string input = Console.ReadLine();
+double number = double.Parse(input);
+number = Math.Abs(number);
+input = number.ToString();
 if (input.Length >= 3)
 {
   Console.WriteLine(input[2]);
